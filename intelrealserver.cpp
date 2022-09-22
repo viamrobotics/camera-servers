90e7c0f40051026082fe89346bbd2574b9acb217
Jeremy Hyde <jeremy@viam.com>
2022-09-22 20:51:54 +0000

    push with fixes for wait_for_frame and alignment issues

--- intelrealserver.cpp
+++ intelrealserver.cpp
@@ -46,33 +46,25 @@ void cameraThread() {
 
         int num = 0;
         for (auto& p : pipelines) {
-            std::cout << "----------------------\n";
 
             std::shared_ptr<CameraOutput> output(new CameraOutput());
 
-            auto start1 = std::chrono::high_resolution_clock::now();
             rs2::frameset frames;
             auto succ = p.try_wait_for_frames(&frames, 1000);
             if (!succ) {
-                std::cout << "Failed to get frame, skipping...\n";
+                DEBUG("Failed to get frame, skipping...");
                 continue;
             }
-            auto finish1 = std::chrono::high_resolution_clock::now();
-            std::cout << " - wait_for_frame: " << std::chrono::duration_cast<std::chrono::milliseconds>(finish1 - start1).count() << "ms\n";
             
             // this handles the geometry so that the
             // x/y of the depth and color are the same
-            auto start2 = std::chrono::high_resolution_clock::now();
-            //frames = alignment.process(frames);
             try {
                 frames = alignment.process(frames);
             } catch (std::exception& e) {
                 // Catch exceptions, since alignment can fail
-                std::cout << "Exception while execution of the processing: " << e.what() << std::endl;
+                std::cout << "Exception while aligning images: " << e.what() << std::endl;
                 continue;
             }
-            auto finish2 = std::chrono::high_resolution_clock::now();
-            std::cout << " - aligment: " << std::chrono::duration_cast<std::chrono::milliseconds>(finish2 - start2).count() << "ms\n";
 
             // do color frame
             auto vf = frames.get_color_frame();
@@ -81,16 +73,12 @@ void cameraThread() {
             assert(vf.get_stride_in_bytes() ==
                    (vf.get_width() * vf.get_bytes_per_pixel()));
 
-            auto start4 = std::chrono::high_resolution_clock::now();
             output->width = vf.get_width();
             output->height = vf.get_height();
             output->ppmdata =
                 my_write_ppm((const char*)vf.get_data(), vf.get_width(),
                              vf.get_height(), vf.get_bytes_per_pixel());
 
-            auto finish4 = std::chrono::high_resolution_clock::now();
-            std::cout << "- ppm: " << std::chrono::duration_cast<std::chrono::milliseconds>(finish4 - start4).count() << "ms\n";
-            auto start5 = std::chrono::high_resolution_clock::now();
             try {
                 output->pic_cv = cv::Mat(vf.get_height(), vf.get_width(),
                                          CV_8UC3, (void*)(vf.get_data()));
@@ -102,8 +90,6 @@ void cameraThread() {
                     << e.what() << std::endl;
                 output->pic_cv = cv::Mat();
             }
-            auto finish5 = std::chrono::high_resolution_clock::now();
-            std::cout << "- try: " << std::chrono::duration_cast<std::chrono::milliseconds>(finish5 - start5).count() << "ms\n";
 
             // create depth maps
 
@@ -118,14 +104,12 @@ void cameraThread() {
 	    int h = output->depth_height;
 	    cv::Mat cvBuf(h, w, CV_16U);
 	    const uint16_t *z_pixels = reinterpret_cast<const uint16_t*>(depth.get_data());
-        auto start3 = std::chrono::high_resolution_clock::now();
 	    for (int y = 0; y < h; y++) {
 	      for (int x = 0; x < w; x++) { 
 		cvBuf.at<uint16_t>(y,x) = z_pixels[y*w+x];
 	      }
 	    }
-        auto finish3 = std::chrono::high_resolution_clock::now();
-        std::cout << "- loop: " << std::chrono::duration_cast<std::chrono::milliseconds>(finish3 - start3).count() << "ms\n";
+
 	    output->depth_cv = cvBuf;
             DEBUG("middle distance: " << depth.get_distance(
                       depth.get_width() / 2, depth.get_height() / 2));
@@ -138,7 +122,6 @@ void cameraThread() {
                                                                     start)
                   .count()
               << "ms");
-        std::cout << "final time: " << std::chrono::duration_cast<std::chrono::milliseconds>(finish - start).count() << "ms\n";
 
         CameraState::get()->ready = 1;
 
